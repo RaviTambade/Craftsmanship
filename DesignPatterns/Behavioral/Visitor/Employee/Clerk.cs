0348b65bdccc6a2bb396b2221bbfda9fdb92c72a
RohitM4926 <rohitmangale4926@gmail.com>
2023-09-02 10:45:37 +0530

    visitor Pattern

---
+++ DesignPatterns/Behavioral/Visitor/Employee/Clerk.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace Transflower.DesignPatterns.Visitor{
+ public class Clerk : Employee
+    {
+        // Constructor
+
+        public Clerk()
+            : base("Ram", 25000.0, 14)
+        {
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/Visitor/Employee/Employees.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace Transflower.DesignPatterns.Visitor{
+    public class Employees
+    {
+        private List<Employee> employees = new List<Employee>();
+
+        public void Attach(Employee employee)
+        {
+            employees.Add(employee);
+        }
+
+        public void Detach(Employee employee)
+        {
+            employees.Remove(employee);
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            foreach (Employee employee in employees)
+            {
+                employee.Accept(visitor);
+            }
+            Console.WriteLine();
+        }
+    }
+}
