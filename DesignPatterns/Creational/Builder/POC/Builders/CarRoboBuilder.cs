d6974d0c5e3b4edbd3eb5a669ff83e10b44e4dbe
Sahil <sahilmankar311@gmail.com>
2023-09-02 15:08:48 +0530

    Builder pattern updated

--- DesignPatterns/Creational/Builder/POC/Builders/CarRoboBuilder.cs
+++ DesignPatterns/Creational/Builder/POC/Builders/CarRoboBuilder.cs
@@ -1,31 +1,31 @@
 namespace Transflower.DesignPatterns.Builder;
 public class CarRoboBuilder:IBuilder{
-    private Car product=null;
-    public void reset(){
-        this.product=new Car();
+    private Vehicle product=null;
+    public void Reset(){
+        this.product=new Vehicle();
         this.product.chassisnumber="654564";
         Console.WriteLine("auto Mode:Chassis is  ready");
     }
-    public void setSeats(int count){
+    public void SetSeats(int count){
 
         this.product.seats=count;
          Console.WriteLine("auto Mode:Seats are mounted");
 
     }
-    public void setEngine(Engine engine){
+    public void SetEngine(Engine engine){
         this.product.engine=engine;
          Console.WriteLine("auto Mode:Engine is set up");
 
     }
-    public void setTripComputer(bool status){
+    public void SetTripComputer(bool status){
         this.product.isTripComputer=status;
          Console.WriteLine("auto Mode:Trip computer set up");
     }
-    public void setGPS(bool status){
+    public void SetGPS(bool status){
         this.product.gps=status;
          Console.WriteLine("auto Mode:GPS is set Up");
     }
-    public Car GetProduct(){
+    public Vehicle GetProduct(){
        // this.reset();
         return product;
     }
--- DesignPatterns/Creational/Builder/POC/Program.cs
+++ DesignPatterns/Creational/Builder/POC/Program.cs
@@ -5,5 +5,5 @@ IBuilder builder=new CarRoboBuilder();
 //IBuilder builder=new CarMannualBuilder();
 //director.ConstructSportsCar(builder);
 director.ConstructSUV(builder);
-Car theCar=builder.GetProduct();
+Vehicle theCar=builder.GetProduct();
 Console.WriteLine(theCar);
