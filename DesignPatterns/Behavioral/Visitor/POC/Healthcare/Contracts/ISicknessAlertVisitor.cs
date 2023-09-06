4443cf1ccde6b2b625b11ff2d32537c6b26c706f
RohitM4926 <rohitmangale4926@gmail.com>
2023-09-06 13:52:02 +0530

    visitor

--- DesignPatterns/Behavioral/Visitor/POC/Healthcare/Contracts/ISicknessAlertVisitor.cs
+++ DesignPatterns/Behavioral/Visitor/POC/Healthcare/Contracts/ISicknessAlertVisitor.cs
@@ -3,7 +3,7 @@ namespace Transflower.DesignPatterns.Visitor.Healthcare.Contracts;
 public interface ISicknessAlertVisitor{
 
     AlertReport Visit(BloodSample blood);
-    AlertReport Visit(XRayImage blood);
-    AlertReport Visit(EcgReading blood);
+    AlertReport Visit(XRayImage image);
+    AlertReport Visit(EcgReading ecg);
 
 }
